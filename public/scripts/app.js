7e94c07d4ee2ddf548ecd6e8f1d0a6077ed6cb39
Paulo Brito <paulocamboim@gmail.com>
2016-10-30 13:02:07 -0700

    Fix: 1. some small bugs on the search. 2. Fix when google place doesn't have an image

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,27 @@
 
+ /**
+ * Toggle spinning gif
+ * @param {string} selector - jquery selector to apply the gif
+ * @param {boolean} visible - Show/Hide spinning gif.
+ */
+
+function isLoading(selector, visible) {
+
+  var spinner = '<img src="/img/loading.gif" class="spinner"/>';
+  if(visible) {
+    $(selector).append(spinner);
+  } else {
+    $(selector +' > img').remove();
+  }
+}
+
+/**
+ * Flash message
+ * @param {string} selector - jquery selector to apply the gif
+ * @param {string} msg - Message to display
+ * @param {boolean} success - depending on this will set background color
+ */
+
 function flashMessage(selector, msg, success) {
   // Change style depding on result
   if (success) {
